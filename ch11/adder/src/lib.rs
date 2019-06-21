6bc3791c57fc1f218324d124f6319e9507bdd984
Kai Moschcau <kai.moschcau@sabre.com>
2019-06-21 13:41:01 +0200

    Condense the adder back down to essential content

--- ch11/adder/src/lib.rs
+++ ch11/adder/src/lib.rs
@@ -1,52 +1,9 @@
-use std::fmt;
-
-#[derive(Debug)]
-pub struct Rectangle {
-    length: u32,
-    width: u32,
-}
-
-impl Rectangle {
-    pub fn can_hold(&self, other: &Rectangle) -> bool {
-        self.length > other.length && self.width > other.width
-    }
-}
-
 pub fn add_two(a: i32) -> i32 {
-    a + 2
+    internal_adder(a, 2)
 }
 
-pub fn greeting(name: &str) -> String {
-    format!("Hello {}!", name)
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        // if value < 1 || value > 100 {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}",
-                value
-            );
-        }
-
-        Guess { value }
-    }
-}
-
-impl fmt::Display for Guess {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "Guess: {}", self.value)
-  }
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -60,61 +17,16 @@ mod tests {
 
     #[test]
     fn add_three_and_two() {
-      assert_eq!(5, add_two(3));
+        assert_eq!(5, add_two(3));
     }
 
     #[test]
-    #[ignore]
     fn one_hundred() {
-      assert_eq!(102, add_two(100));
+        assert_eq!(102, add_two(100));
     }
 
     #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            length: 8,
-            width: 7,
-        };
-        let smaller = Rectangle {
-            length: 5,
-            width: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            length: 8,
-            width: 7,
-        };
-        let smaller = Rectangle {
-            length: 5,
-            width: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{}`",
-            result
-        );
-    }
-
-    #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100_panics() {
-        Guess::new(200);
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
     }
 }
