fd8d54cf9c61d4cf755f25124c6f738d531e9257
Kai Moschcau <mail@kmoschcau.de>
2019-12-01 15:08:08 +0100

    Update ch12 with changes from ch13

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -10,15 +11,31 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String], case_sensitive: bool) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name")
+        };
+
+        Ok(Config { query, filename, case_sensitive: Self::case_sensitive(args) })
+    }
+
+    fn case_sensitive(args: env::Args) -> bool {
+        let options: Vec<String> = args.collect();
+        if options.contains(&"--no-ignore-case".to_string()) {
+            return true;
         }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename, case_sensitive })
+        if options.contains(&"--ignore-case".to_string()) {
+            return false;
+        }
+        env::var("CASE_INSENSITIVE").is_err()
     }
 }
 
@@ -39,59 +56,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+            .filter(|line| line.contains(query))
+            .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
-}
-
-#[cfg(test)]
-mod config_new {
-    use super::*;
-
-    #[test]
-    fn one_argument_return_err() {
-        let vec = vec!["minigrep".to_string()];
-
-        assert_eq!(Err("not enough arguments"), Config::new(&vec, false));
-    }
-
-    #[test]
-    fn two_arguments_return_err() {
-        let vec = vec!["minigrep".to_string(), "foo".to_string()];
-
-        assert_eq!(Err("not enough arguments"), Config::new(&vec, false));
-    }
-
-    #[test]
-    fn three_arguments_return_ok_with_config() {
-        let vec = vec!["minigrep".to_string(), "foo".to_string(), "bar".to_string()];
-
-        assert_eq!(Ok(Config {
-                          query: "foo".to_string(),
-                          filename: "bar".to_string(),
-                          case_sensitive: false
-                      }),
-                   Config::new(&vec, false));
-    }
+    let lowercase_query = query.to_lowercase();
+    contents.lines()
+            .filter(|line| line.to_lowercase().contains(&lowercase_query))
+            .collect()
 }
 
 #[cfg(test)]
--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -4,10 +4,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let case_sensitive = case_sensitive(&args);
-
-    let config = Config::new(&args, case_sensitive).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -17,14 +14,4 @@ fn main() {
 
         process::exit(1);
     }
-}
-
-fn case_sensitive(args: &[String]) -> bool {
-    if args.contains(&"--no-ignore-case".to_string()) {
-        return true;
-    }
-    if args.contains(&"--ignore-case".to_string()) {
-        return false;
-    }
-    env::var("CASE_INSENSITIVE").is_err()
 }
